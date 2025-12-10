3bc6c88f8715a34b8dfd69e97524a15b7415d2f3
amrxtgh <152877271+amrxtgh69@users.noreply.github.com>
2025-12-10 18:46:40 +0545

    Time: 0 ms (100%), Space: 2.3 MB (86.77%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.rs
@@ -0,0 +1,12 @@
+impl Solution {
+    pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
+        let mut i = 1;
+        for j in 1..nums.len() {
+            if nums[j] != nums[j - 1] {
+                nums[i] = nums[j];
+                i += 1; 
+            }
+        }
+        i as i32
+    }
+}
