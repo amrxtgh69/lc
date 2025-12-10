bc18cadd6739e33e24b28aadc6312676ffd54bd4
amrxtgh <152877271+amrxtgh69@users.noreply.github.com>
2025-12-10 17:57:45 +0545

    Time: 0 ms (100%), Space: 2.2 MB (97.76%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.rs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.rs
@@ -2,8 +2,8 @@ impl Solution {
     pub fn longest_common_prefix(mut strs: Vec<String>) -> String {
         //sorting the arrayz
         strs.sort();
-        let first = strs[0].clone();
-        let last = strs[strs.len() - 1].clone();
+        let first = &strs[0];
+        let last = &strs[strs.len() - 1];
 
         let first_bytes = first.as_bytes();
         let last_bytes = last.as_bytes();
